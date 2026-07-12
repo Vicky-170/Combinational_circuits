b0d851f7ce78c507f6b7555e15e96ff79fb0381b
Vicky-170 <vicky93588@gmail.com>
2026-07-12 22:07:33 +0530

    Add files via upload

---
+++ Full Adder/tb_full_adder.v
@@ -0,0 +1,27 @@
+module tb_full_adder;
+    reg A;
+    reg B;
+    reg Cin;
+    wire Sum;
+    wire Cout;
+
+    FULL_ADDER uut (
+        .A(A),
+        .B(B),
+        .Cin(Cin),
+        .Sum(Sum),
+        .Cout(Cout)
+    );
+
+    initial begin
+        A = 0; B = 0; Cin = 0; #10;
+        A = 0; B = 0; Cin = 1; #10;
+        A = 0; B = 1; Cin = 0; #10;
+        A = 0; B = 1; Cin = 1; #10;
+        A = 1; B = 0; Cin = 0; #10;
+        A = 1; B = 0; Cin = 1; #10;
+        A = 1; B = 1; Cin = 0; #10;
+        A = 1; B = 1; Cin = 1; #10;
+        $finish;
+    end
+endmodule
