e1b740e181c1fedf99e7c3952db4f739f94f18f3
Vicky-170 <vicky93588@gmail.com>
2026-07-12 22:06:27 +0530

    Add files via upload

---
+++ Half Adder/tb_half_adder.v
@@ -0,0 +1,14 @@
+`timescale 1ns/1ps
+module tb_half_adder;
+  reg A, B;
+  wire Sum, Carry;
+  half_adder uut(.A(A), .B(B), .Sum(Sum), .Carry(Carry));
+  initial begin
+    $monitor("A=%b B=%b Sum=%b Carry=%b", A, B, Sum, Carry);
+    A=0; B=0; #10;
+    A=0; B=1; #10;
+    A=1; B=0; #10;
+    A=1; B=1; #10;
+    $finish;
+  end
+endmodule
